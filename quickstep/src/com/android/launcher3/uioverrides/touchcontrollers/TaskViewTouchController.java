7736a65e0835119a629f88bd399e58f07f415daf
xiaoyang.wang <xiaoyang.wang@T2Mobile.com>
2022-02-15 16:35:56 +0800

    FP4-3456 [Monkey][Crash]com.android.launcher3:java.lang.NullPointerException

--- quickstep/src/com/android/launcher3/uioverrides/touchcontrollers/TaskViewTouchController.java
+++ quickstep/src/com/android/launcher3/uioverrides/touchcontrollers/TaskViewTouchController.java
@@ -339,6 +339,11 @@ public abstract class TaskViewTouchController<T extends BaseDraggingActivity>
             fling = false;
         }
         PagedOrientationHandler orientationHandler = mRecentsView.getPagedOrientationHandler();
+        if (mCurrentAnimation == null) {
+            reInitAnimationController(false);
+            mDisplacementShift = 0;
+            return;
+        }
         float progress = mCurrentAnimation.getProgressFraction();
         float interpolatedProgress = mCurrentAnimation.getInterpolatedProgress();
         if (fling) {

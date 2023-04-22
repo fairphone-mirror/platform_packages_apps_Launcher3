55f504e1b292db2bedaca7a226924200db2cc32a
tianhai.sun <tianhai.sun@t2mobile.com>
2023-04-22 15:09:35 +0800

    [2][FP5-101][FP5][Charging]Battery Charging Speed Options
    Change-Id: I35a700f24144b265545fe93b83ae4c6be385f476

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -254,6 +254,10 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
+import android.os.SystemProperties;
+import android.provider.Settings;
+
+
 /**
  * Default launcher application.
  */
@@ -590,6 +594,16 @@ public class Launcher extends StatefulActivity<LauncherState>
         }
         setTitle(R.string.home_screen);
         mStartupLatencyLogger.logEnd(LAUNCHER_LATENCY_STARTUP_ACTIVITY_ON_CREATE);
+        
+        try {
+            if ("1".equals(SystemProperties.get("persist.sys.is_first_boot"))) {
+                Settings.Global.putStringForUser(getContentResolver(),
+                        Settings.Global.SET_BATTERY_CHARGING_MODE, "yes",
+                        UserHandle.myUserId());
+            }
+        }catch (Exception e){
+            Log.i("sth__","    Exception :" + e);
+        }
     }
 
     /**

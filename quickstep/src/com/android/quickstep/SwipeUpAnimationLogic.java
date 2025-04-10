4764c179398b025b4639c467a5029185f81a36de
haibiao.lu <haibiao.lu@t2mobile.com>
2025-04-10 09:51:15 +0800

    [FPS-1922][FPS-1915]fix launcher crash during monkey test
    * Root Cause
      crash
    * Solution
      Fix code to meet customer's requestment
    * Test Steps
      launcher
    
    Change-Id: I932b43b295bbfee78712cfaf2f1a255e2b293c7a

--- quickstep/src/com/android/quickstep/SwipeUpAnimationLogic.java
+++ quickstep/src/com/android/quickstep/SwipeUpAnimationLogic.java
@@ -490,12 +490,16 @@ public abstract class SwipeUpAnimationLogic implements
                 return;
             }
 
-            mTargetTaskView.setScaleX(scale);
-            mTargetTaskView.setScaleY(scale);
-            mTargetTaskView.setTranslationX(
-                    currentRect.centerX() - mThumbnailStartBounds.centerX());
-            mTargetTaskView.setTranslationY(
-                    currentRect.centerY() - mThumbnailStartBounds.centerY());
+            try{
+                mTargetTaskView.setScaleX(scale);
+                mTargetTaskView.setScaleY(scale);
+                mTargetTaskView.setTranslationX(
+                        currentRect.centerX() - mThumbnailStartBounds.centerX());
+                mTargetTaskView.setTranslationY(
+                        currentRect.centerY() - mThumbnailStartBounds.centerY());
+            }catch (IllegalArgumentException e) {
+                //do nothing
+            }
         }
 
         @Override

7cdf175788d9fbe019cbd5bcffdb5b50a4bc8ae8
yubinying <yubin.ying@t2mobile.com>
2025-04-10 14:17:06 +0800

    [FPS-1888][Launcher]The app downloaded from the play store is displayed on the main screen by default
    * Root Cause
      download app display icon in main screen
    * Solution
      remove  QSB_ON_FIRST_SCREEN in judgement
    * Test Steps
      follow jira
    
    Change-Id: Ic517558b9ae2fe8f840cffaf4a25c0c65f623cad

--- src/com/android/launcher3/model/WorkspaceItemSpaceFinder.java
+++ src/com/android/launcher3/model/WorkspaceItemSpaceFinder.java
@@ -67,8 +67,8 @@ public class WorkspaceItemSpaceFinder {
         int screenCount = workspaceScreens.size();
         // First check the preferred screen.
         IntSet screensToExclude = new IntSet();
-        if (FeatureFlags.QSB_ON_FIRST_SCREEN
-                && !SHOULD_SHOW_FIRST_PAGE_WIDGET) {
+        if (/*FeatureFlags.QSB_ON_FIRST_SCREEN
+                &&*/ !SHOULD_SHOW_FIRST_PAGE_WIDGET) {
             screensToExclude.add(FIRST_SCREEN_ID);
         }
 

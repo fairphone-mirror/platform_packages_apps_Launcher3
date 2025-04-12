8c416c953dd5310191533defb3e951f737a54183
haibiao.lu <haibiao.lu@t2mobile.com>
2025-04-12 11:33:53 +0800

    [FPS-1959]fix launcher3 crash during monkey test
    * Root Cause
      monkey
    * Solution
      Fix code to meet customer's requestment
    * Test Steps
      launcher
    
    Change-Id: I120ad36ecf68333b45edee7f9407d8a350b6b23e

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -2671,6 +2671,10 @@ public abstract class AbsSwipeUpHandler<
                 transformParams.createSurfaceParams(taskViewSimulator);
         SurfaceControl.Transaction transaction = surfaceTransaction.getTransaction();
 
+        if(transaction == null){
+            return false;
+        }
+
         for (RemoteAnimationTarget app : targets.apps) {
             transaction.setAlpha(app.leash, 1f - fadeProgress);
             transaction.setPosition(app.leash,

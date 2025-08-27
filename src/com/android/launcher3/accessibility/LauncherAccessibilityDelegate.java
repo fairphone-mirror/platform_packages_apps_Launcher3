4cfa04fd43e5bc2ae4b1d9742075d3def71bb74d
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2025-08-27 14:12:36 +0800

    Saving 'app pair' causes Quickstep to stop/quit
    Test Steps:
    1. Open 'App overview' to see recent apps.
    2. At the top, tap and hold the most recent app's icon.
    3. Tap Split screen and choose a second app to enter Split
    
    Issue: FP5#873
    Issue: FP5V-1096
    Issue: FP4-A15#384
    Change-Id: I95b33c34418439883ed69267e122d4b4783dc66d
    (cherry picked from commit eacc4c467a9b4578cafc709cc1212e4836c577f2)
    (cherry picked from commit a79936a5b9a4e23340da926fbb857007642ca796)

--- src/com/android/launcher3/accessibility/LauncherAccessibilityDelegate.java
+++ src/com/android/launcher3/accessibility/LauncherAccessibilityDelegate.java
@@ -12,6 +12,7 @@ import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_NOT_PINNABL
 
 import android.animation.AnimatorSet;
 import android.appwidget.AppWidgetProviderInfo;
+import android.content.ComponentName;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -365,8 +366,9 @@ public class LauncherAccessibilityDelegate extends BaseAccessibilityDelegate<Lau
         CellLayout layout = (CellLayout) workspace.getPageAt(screenIndex);
 
         boolean found = layout.findCellForSpan(outCoordinates, info.spanX, info.spanY);
+        ComponentName mComponentName = info.getTargetComponent();
 
-        if ("com.orange.update.widget.ComboFolderWidgetProvider".equals(info.getTargetComponent().getClassName()) && isOrangeOperator) {
+        if (mComponentName != null && "com.orange.update.widget.ComboFolderWidgetProvider".equals(mComponentName.getClassName()) && isOrangeOperator) {
             if (screenId == 0) {
                 found = layout.findCellForSpan(outCoordinates,Integer.valueOf(operatorAppPackageListForOrange[0][2])/*cellX*/,Integer.valueOf(operatorAppPackageListForOrange[0][3])/*cellY*/, info.spanX, info.spanY);
             } else {
@@ -379,7 +381,7 @@ public class LauncherAccessibilityDelegate extends BaseAccessibilityDelegate<Lau
             screenId = workspaceScreens.get(screenIndex);
             layout = (CellLayout) workspace.getPageAt(screenIndex);
             found = layout.findCellForSpan(outCoordinates, info.spanX, info.spanY);
-            if ("com.orange.update.widget.ComboFolderWidgetProvider".equals(info.getTargetComponent().getClassName()) && isOrangeOperator) {
+            if (mComponentName != null && "com.orange.update.widget.ComboFolderWidgetProvider".equals(mComponentName.getClassName()) && isOrangeOperator) {
                 if (screenId == 0) {
                     found = layout.findCellForSpan(outCoordinates,Integer.valueOf(operatorAppPackageListForOrange[0][2])/*cellX*/,Integer.valueOf(operatorAppPackageListForOrange[0][3])/*cellY*/, info.spanX, info.spanY);
                 } else {
@@ -403,7 +405,7 @@ public class LauncherAccessibilityDelegate extends BaseAccessibilityDelegate<Lau
         screenId = emptyScreenIds.getArray().get(0);
         layout = workspace.getScreenWithId(screenId);
         found = layout.findCellForSpan(outCoordinates, info.spanX, info.spanY);
-        if ("com.orange.update.widget.ComboFolderWidgetProvider".equals(info.getTargetComponent().getClassName()) && isOrangeOperator) {
+        if (mComponentName != null && "com.orange.update.widget.ComboFolderWidgetProvider".equals(mComponentName.getClassName()) && isOrangeOperator) {
             if (screenId == 0) {
                 found = layout.findCellForSpan(outCoordinates,Integer.valueOf(operatorAppPackageListForOrange[0][2])/*cellX*/,Integer.valueOf(operatorAppPackageListForOrange[0][3])/*cellY*/, info.spanX, info.spanY);
             } else {

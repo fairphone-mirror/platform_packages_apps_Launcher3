8e155216b0c7b4f313abe307f7905f59d6663dc9
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2026-01-22 11:14:13 +0800

    FPSW-732 [FPS #313]Wrong app is opening
    * Comment
      When searching for an app from the app drawer, if you click quick enough a different app will open
    * Root Cause
      The launcher search animation is too slow, so when users operate quickly, they end up opening different apps.
    * Solution
      Reduce animation time and do not display the app information of Head during search
    * Test Steps
      1.Open the app drawer  2.Search for an app  3.Very quickly select the app once it appears  4.The wrong app will open
    
    Change-Id: I8edb396b73f6fc41b1268428fa18b9ca8413a2b1
    (cherry picked from commit c9d3186b372c9240a411a08dac285febb38f2ea8)

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -120,7 +120,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
     public static final float PULL_MULTIPLIER = .02f;
     public static final float FLING_VELOCITY_MULTIPLIER = 1200f;
     protected static final String BUNDLE_KEY_CURRENT_PAGE = "launcher.allapps.current_page";
-    private static final long DEFAULT_SEARCH_TRANSITION_DURATION_MS = 300;
+    private static final long DEFAULT_SEARCH_TRANSITION_DURATION_MS = 0;
     // Render the header protection at all times to debug clipping issues.
     private static final boolean DEBUG_HEADER_PROTECTION = false;
     /** Context of an activity or window that is inflating this container. */
@@ -738,7 +738,9 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
     void setupHeader() {
         mAdditionalHeaderRows.forEach(row -> mHeader.onPluginDisconnected(row));
 
-        mHeader.setVisibility(View.VISIBLE);
+        if (!isSearching()) {
+            mHeader.setVisibility(View.VISIBLE);
+        }
         boolean tabsHidden = !mUsingTabs;
         mHeader.setup(
                 mAH.get(AdapterHolder.MAIN).mRecyclerView,
